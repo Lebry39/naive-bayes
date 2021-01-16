396918451f8d1bf499b8b404b0a881a116ff72d4
lebry39 <amexy39@yahoo.co.jp>
2021-01-16 23:27:39 +0900

    推論のパフォーマンス改善とリファクタリング

--- bayes.py
+++ bayes.py
@@ -6,42 +6,78 @@ import re
 import unicodedata
 import MeCab  # https://pypi.org/project/mecab-python3/
 
-wakati = MeCab.Tagger("-Owakati")
+# sudo apt-get install mecab mecab-ipadic-utf8
+# -d /var/lib/mecab/dic/ipadic-utf8
+wakati = MeCab.Tagger("-Owakati -d /var/lib/mecab/dic/ipadic-utf8")
 
 
 class NaiveBayes:
     def __init__(self):
-        # 学習された単語の集合
-        self.vocabularies = set()
-        self.word_count = {}
+        # ラプラススムージングのパラメーター
+        self.smoothing_a = 0.01
+        self.smoothing_b = 0.02
 
-        self.category_count = {}
-        self.category_words = {}
+        # 登録された単語の集合
+        self._vocabularies = set()
+
+        # ワードの出現数
+        # {"wordA": 15, "wordB": 2, ...}
+        self._word_count = {}
+
+        # カテゴリごとのドキュメントの登録数
+        # {"CategoryA": 16, "CategoryB": 9}
+        self._category_count = {}
+
+        # カテゴリごとのワードの出現数
+        # {"CategoryA": {"wordA": 12, "wordB": 1}, ...}
+        self._category_words = {}
+
+        # 事前に計算されたワードスコア
+        # {"CategoryA": {"wordA": -902, "wordB": -122}, ...}
+        self._calculated_word_scores = {}
 
     def fit(self, category: str, texts: list):
-        if category in self.category_count:
+        if category in self._category_count:
             raise Exception("%s is already registered" % category)
 
-        self.category_count[category] = len(texts)
+        # キャッシュを初期化
+        self._calculated_word_scores = None
+        self._category_count[category] = len(texts)
 
-        concatenated_text = " ".join(texts)
-        words = self.to_words(concatenated_text)
+        words = []
+        for text in texts:
+            words += self.to_words(text)
         counts = self.count_words(words)
 
-        # 最低でもn回出たら学習する
-        min_word_count = 1
-        if min_word_count >= 2:
-            counts = {k: v for k, v in counts.items() if v >= min_word_count}
-            words = [w for w in words if w in set(counts.keys())]
-
         vocabulary = set(words)
-        self.vocabularies |= vocabulary
-        self.category_words[category] = counts
+        self._vocabularies |= vocabulary
+        self._category_words[category] = counts
 
         for w in vocabulary:
-            if w not in self.word_count:
-                self.word_count[w] = 0
-            self.word_count[w] += self.category_words[category][w]
+            if w not in self._word_count:
+                self._word_count[w] = 0
+            self._word_count[w] += self._category_words[category][w]
+
+    def pre_calculate_word_score(self):
+        self._calculated_word_scores = {}
+
+        for category in self._category_count:
+            evidence = float(
+                sum(self._category_words[category].values())
+                + len(self._vocabularies) * self.smoothing_b
+            )
+            self._calculated_word_scores[category] = {}
+
+            for word in self._category_words[category]:
+                word_score = np.log10((
+                    self._get_word_count(category, word) +
+                    self.smoothing_a
+                ) / evidence)
+
+                self._calculated_word_scores[category][word] = word_score
+
+            nohit_score = np.log10(self.smoothing_a / evidence)
+            self._calculated_word_scores[category][None] = nohit_score
 
     def count_words(self, words: list) -> dict:
         counts = {k: 0 for k in set(words)}
@@ -56,13 +92,13 @@ class NaiveBayes:
         return words
 
     def _normalize_document(self, text: str) -> str:
-        t = unicodedata.normalize("NFKC", text)
+        t = re.sub(r'\s+', r' ', text)
+        t = unicodedata.normalize("NFKC", t)
         t = t.lower()
-        t = re.sub(r'\s+', r' ', t)
         return t
 
     def _remove_dc_words(self, words: list) -> list:
-        discard_words = r"(\s+)"  # |[,.()\[\]（）「」'\"、。]
+        discard_words = r"\s+"
         words = [w for w in words if not re.match(discard_words, w)]
         return words
 
@@ -70,12 +106,15 @@ class NaiveBayes:
         return wakati.parse(text).split()
 
     def classify(self, text: str) -> dict:
+        if self._calculated_word_scores is None:
+            self.pre_calculate_word_score()
+
         words = self.to_words(text)
         counts = self.count_words(words)
 
         category_scores = {}
-        for category in self.category_count.keys():
-            score = np.log(self._category_p(category))
+        for category in self._category_count:
+            score = np.log10(self._category_p(category))
             score += self._calc_text_score(category, counts)
             category_scores[category] = score
 
@@ -88,24 +127,24 @@ class NaiveBayes:
         return p
 
     def _category_p(self, category: str) -> float:
-        return self.category_count[category] / sum(self.category_count.values())
+        return self._category_count[category] / sum(self._category_count.values())
 
     def _calc_text_score(self, category: str, word_counts: dict) -> int:
-        evidence = float(
-            sum(self.category_words[category].values())
-            + len(self.vocabularies)
-        )
-
         score = 0
-        for word in word_counts.keys():
-            p = (self._get_word_count(category, word) + 1.0) / evidence
-            score += np.log(p) * word_counts[word]
+        for word in word_counts:
+            # Word hit
+            if word in self._calculated_word_scores[category]:
+                word_score = self._calculated_word_scores[category][word]
+            else:
+                word_score = self._calculated_word_scores[category][None]
+
+            score += word_score * word_counts[word]
 
         return score
 
     def _get_word_count(self, category: str, word: str) -> float:
-        if word in self.category_words[category]:
-            return float(self.category_words[category][word])
+        if word in self._category_words[category]:
+            return float(self._category_words[category][word])
         return 0.0
 
 
@@ -138,7 +177,7 @@ def main():
         p = obj.classify(doc["text"])
         spam_rate = p["spam"]
 
-        is_spam = spam_rate > 0.5
+        is_spam = spam_rate > 0.99
 
         if doc["class"] == "spam":
             if is_spam:
@@ -167,7 +206,19 @@ def main():
 
 
 if __name__ == "__main__":
-    for _ in range(10):
-        main()
-        print()
-        break
+    obj = NaiveBayes()
+    obj.fit("マック", [
+        "ハンバーガー ポテト ジュース",
+        "ハンバーガー チキン ジュース",
+        "ハンバーガー",
+        "ポテト チキン",
+    ])
+    obj.fit("ケンタッキー", [
+        "チキン ポテト ジュース",
+        "ハンバーガー ポテト チキン",
+        "ジュース チキン チキン",
+        "ハンバーガー チキン チキン ポテト ビスケット ジュース"
+    ])
+
+    print(obj.classify("ポテト ハンバーガー ジュース"))
+    print(obj.classify("チキン"))
